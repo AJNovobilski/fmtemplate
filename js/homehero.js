750957a7460fd087107ee09c5bf40bd5c2770e69
anovobilskischool <anovobilskischool@gmail.com>
2021-03-14 23:09:37 -0700

    init

---
+++ js/homehero.js
@@ -0,0 +1,10 @@
+$(function() {
+    $(".text").typed({
+        strings: ["Websites", "Logos", "SEO", "Fliers", "Brochures", "Product Packaging", "Graphic Design", "Animation", "Business Cards"  ],
+        typeSpeed: 44,
+        backSpeed: 55,
+        backDelay: 1909,
+        showCursor: false,
+        loop: true
+      });
+    });
